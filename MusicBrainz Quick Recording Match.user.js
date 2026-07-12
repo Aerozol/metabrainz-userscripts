f155644aa277d27ec1198f93c1402cd10ceb9ea8
chaban <225617645+chaban-mb@users.noreply.github.com>
2026-07-12 08:06:17 +0200

    chore(quick-recording-match): bump version to 5.21

--- MusicBrainz Quick Recording Match.user.js
+++ MusicBrainz Quick Recording Match.user.js
@@ -2,7 +2,7 @@
 // @name        MusicBrainz Quick Recording Match
 // @namespace   https://github.com/Aerozol/metabrainz-userscripts
 // @description Select the first recording search result for each track, in the release editor Recordings tab.
-// @version     5.20
+// @version     5.21
 // @downloadURL https://raw.githubusercontent.com/Aerozol/metabrainz-userscripts/master/MusicBrainz%20Quick%20Recording%20Match.user.js
 // @updateURL   https://raw.githubusercontent.com/Aerozol/metabrainz-userscripts/master/MusicBrainz%20Quick%20Recording%20Match.user.js
 // @license     MIT
